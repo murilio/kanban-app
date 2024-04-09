c37e8fc165e943670eae28338e179cb4511c98ac
murilio <murilio.nssw@gmail.com>
2024-04-09 16:21:43 -0300

    chore: criando o mock inicial da aplicação

---
+++ src/app/mock/api.mock.ts
@@ -0,0 +1,58 @@
+export const APIMOCK = [
+  {
+    title: "To Do",
+    type: "to-do",
+    options: [
+      {
+        title: "Brainstorming",
+        description:
+          "Brainstorming brings team members' diverse experience into play.",
+        priority: "Low",
+      },
+      {
+        title: "Wireframes",
+        description:
+          "Low fidelity wireframes include the most basic content and visuals.",
+        priority: "Medium",
+      },
+      {
+        title: "Research",
+        description:
+          "User research helps you to create an optimal product for users.",
+        priority: "High",
+      },
+    ],
+  },
+  {
+    title: "On Progress",
+    type: "on-progress",
+    options: [
+      {
+        title: "Onboarding Illustrations ",
+        description: "Onboarding Illustrations ",
+        priority: "Low",
+      },
+      {
+        title: "Moodboard",
+        description: "Moodboard",
+        priority: "Medium",
+      },
+    ],
+  },
+  {
+    title: "Done",
+    type: "done",
+    options: [
+      {
+        title: "Mobile App Design",
+        description: "Mobile App Design",
+        priority: "Completed",
+      },
+      {
+        title: "Design System",
+        description: "It just needs to adapt the UI from what you did before ",
+        priority: "Completed",
+      },
+    ],
+  },
+];
