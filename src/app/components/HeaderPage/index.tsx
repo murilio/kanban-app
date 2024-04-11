aa5226f73fe44752f5adbdebdc6aad68c8f27add
murilio <murilio.nssw@gmail.com>
2024-04-11 01:10:15 -0300

    chore: estilizando o header da página

--- src/app/components/HeaderPage/index.tsx
+++ src/app/components/HeaderPage/index.tsx
@@ -1,11 +1,15 @@
 import { FiPlus } from "react-icons/fi";
 
-export const HeaderPage = () => {
+interface IHeaderPageProps {
+  onClick: () => void
+}
+
+export const HeaderPage = ({ onClick }: IHeaderPageProps) => {
   return (
     <div className="flex justify-between my-6">
       <h2 className="text-4xl font-bold">Project X</h2>
 
-      <button className="flex items-center gap-2 h-9 bg-indigo-100 px-2 rounded border-indigo-500 border text-sm transition hover:bg-indigo-500 hover:text-slate-50"><FiPlus />To Do</button>
+      <button onClick={onClick} className="flex items-center gap-2 h-9 bg-indigo-100 px-2 rounded border-indigo-500 border text-sm transition hover:bg-indigo-500 hover:text-slate-50"><FiPlus />To Do</button>
     </div>
   )
 }
