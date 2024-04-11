9f066f48f0e037993ba841eb5de50072743b39a0
murilio <murilio.nssw@gmail.com>
2024-04-11 09:40:44 -0300

    style: atualizando style do header

--- src/app/components/HeaderPage/index.tsx
+++ src/app/components/HeaderPage/index.tsx
@@ -6,9 +6,9 @@ interface IHeaderPageProps {
 
 export const HeaderPage = ({ onClick }: IHeaderPageProps) => {
   return (
-    <div className="flex justify-between my-6">
+    <div className="flex items-center my-6">
       <h2 className="text-4xl font-bold">Project X</h2>
-
+      <span className="block mx-8 min-h-10 w-[1px] bg-slate-200" />
       <button onClick={onClick} className="flex items-center gap-2 h-9 bg-indigo-100 px-2 rounded border-indigo-500 border text-sm transition hover:bg-indigo-500 hover:text-slate-50"><FiPlus />To Do</button>
     </div>
   )
